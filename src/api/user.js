73a1a93d080b27300faf78687ce1aa452778cbfe
Moto Chowdhury <motochowdhury@gmail.com>
2022-11-30 15:10:55 +0600

    Delete Seller API done

--- src/api/user.js
+++ src/api/user.js
@@ -19,14 +19,3 @@ export const deleteSeller = (id, email, refetch) => {
       toast.success(err.message);
     });
 };
-
-export const verifySeller = (email, refetch) => {
-  fetch(`${process.env.REACT_APP_SERVER_API}/admin/seller?email=${email}`, {
-    method: "PATCH",
-    headers: {
-      authorization: `bearar ${localStorage.getItem("access-token")}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => refetch());
-};
