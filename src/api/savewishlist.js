2af55a4d0b24de41cd087219bea5d8360e524d8f
Moto Chowdhury <motochowdhury@gmail.com>
2022-11-30 15:06:06 +0600

    Book product api is done

--- src/api/savewishlist.js
+++ src/api/savewishlist.js
@@ -11,3 +11,27 @@ export const saveWishList = (wishProduct) => {
     .then((data) => toast.success("Added In Wishlist"))
     .catch((err) => toast.error(err));
 };
+
+export const bookWishProd = (id, prod, refetch) => {
+  delete prod?._id;
+  fetch(`${process.env.REACT_APP_SERVER_API}/wishlist?id=${id}`, {
+    method: "DELETE",
+    headers: {
+      "application-type": "application/json",
+      authorization: `bearar ${localStorage.getItem("access-token")}`,
+    },
+    body: JSON.stringify(prod),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      axios
+        .post(`${process.env.REACT_APP_SERVER_API}/booking`, prod, {
+          headers: {
+            authorization: `bearar ${localStorage.getItem("access-token")}`,
+          },
+        })
+        .then((data) => toast.success("Booking Successfull"))
+        .catch((err) => toast.error(err));
+      refetch();
+    });
+};
