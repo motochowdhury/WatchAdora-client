8118be678058c038229ad6c3bb6578507300d618
Moto Chowdhury <motochowdhury@gmail.com>
2022-11-30 19:30:24 +0600

    Longin problem

--- src/routes/RequireAuth.jsx
+++ src/routes/RequireAuth.jsx
@@ -3,16 +3,14 @@ import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import Loader from "../components/Loader";
 import { AuthContext } from "../contexts/AuthProvider";
-import useUser from "../hook/useUser";
 
 const RequireAuth = ({ children }) => {
   const { user: currUser, loading } = useContext(AuthContext);
-  const [user, processing] = useUser(currUser?.email);
   const location = useLocation();
-  if (loading || processing) {
+  if (loading) {
     return <Loader />;
   }
-  if (currUser?.email && user?.email) {
+  if (currUser && currUser?.email) {
     return children;
   }
   return <Navigate to="/login" state={{ from: location }} replace />;
