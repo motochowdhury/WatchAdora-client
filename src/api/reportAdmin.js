6fd3578e1051acbc8646b18816fad338c9989bc2
Moto Chowdhury <motochowdhury@gmail.com>
2022-11-30 15:15:25 +0600

    Report to Admin APi done

--- src/api/reportAdmin.js
+++ src/api/reportAdmin.js
@@ -11,21 +11,3 @@ export const reportAdmin = (reportProdudct) => {
     .then((data) => toast.success("Reported Successfully"))
     .catch((err) => toast.error(err));
 };
-
-export const deleteReportedProd = (id, prodId, refetch) => {
-  fetch(
-    `${process.env.REACT_APP_SERVER_API}/report?id=${id}&productId=${prodId}`,
-    {
-      method: "DELETE",
-      headers: {
-        authorization: `bearar ${localStorage.getItem("access-token")}`,
-      },
-    }
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      toast.success("Product Deleted");
-      refetch();
-    })
-    .catch((err) => toast.error(err.message));
-};
